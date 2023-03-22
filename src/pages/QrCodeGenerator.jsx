0c734d7857fd984b6f1be093cdc0a7643940fc3d
AjarnDang <torntan.j@gmail.com>
2023-03-22 11:25:22 +0700

    Update QrCodeGenerator.jsx

--- src/pages/QrCodeGenerator.jsx
+++ src/pages/QrCodeGenerator.jsx
@@ -63,52 +63,7 @@ function QrCodeGenerator() {
 
   return (
     <body className="signup-body">
-      <div className="container">
-        <div className="signup-form d-flex align-items-center justify-content-center">
-          <div className="card border-0">
-            <div className="card-header">
-              <h3 className="mb-0">QR Code Generator</h3>
-            </div>
-            <div className="card-body p-3">
-              <div class="form-group row">
-                <div class="col-md-auto my-2">
-                  <input
-                    type="email"
-                    name="email"
-                    class="form-control"
-                    placeholder="Enter your email"
-                    onChange={(event) => setEmail(event.target.value)}
-                    required
-                  />
-                </div>
-                <div className="col-md-auto my-2">
-                  <button class=" btn btn-primary" onClick={getQRCode}>
-                    Confirm
-                  </button>
-                </div>
-              </div>
-              <div className="row mt-3">
-                <div className="col-md-auto" id="qrCodeEl">
-                  {qrcode && (
-                    <>
-                      <img src={qrcode} width="500px" />
-                      <br />
-                      <a
-                        id=""
-                        className="btn btn-success mt-2"
-                        href={qrcode}
-                        download="qrcode.png"
-                      >
-                        Download
-                      </a>
-                    </>
-                  )}
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      
     </body>
   );
 }
